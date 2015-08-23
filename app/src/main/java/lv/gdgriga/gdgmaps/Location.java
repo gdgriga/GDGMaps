c4f7909cb5bb93ba503db361c34ff7bb6dc4866d
Valentin-Paramonov <valentine.paramonov@gmail.com>
2015-08-23 18:08:30 +0300

    Setup the map fragment and set the default location to zoom to

--- app/src/main/java/lv/gdgriga/gdgmaps/Location.java
+++ app/src/main/java/lv/gdgriga/gdgmaps/Location.java
@@ -1,5 +1,8 @@
 package lv.gdgriga.gdgmaps;
 
+import com.google.android.gms.maps.model.LatLng;
+
 public class Location {
     public static final float DEFAULT_ZOOM = 13;
+    public static final LatLng RIGA = new LatLng(56.948889, 24.106389);
 }
--- app/src/main/java/lv/gdgriga/gdgmaps/photo_view/PhotoViewMapFragment.java
+++ app/src/main/java/lv/gdgriga/gdgmaps/photo_view/PhotoViewMapFragment.java
@@ -1,6 +1,24 @@
 package lv.gdgriga.gdgmaps.photo_view;
 
-import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.*;
+
+import lv.gdgriga.gdgmaps.Location;
 
 public class PhotoViewMapFragment extends MapFragment {
+    private GoogleMap map;
+
+    private OnMapReadyCallback onMapReady = new OnMapReadyCallback() {
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            map = googleMap;
+            map.getUiSettings().setMapToolbarEnabled(false);
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(Location.RIGA, Location.DEFAULT_ZOOM));
+        }
+    };
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        getMapAsync(onMapReady);
+    }
 }
