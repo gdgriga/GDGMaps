b75d7329943a318a9fe238fa396ef136b3f0f7c6
Valentin-Paramonov <valentine.paramonov@gmail.com>
2015-08-25 22:37:14 +0300

    Set up tag fragment

--- app/src/main/java/lv/gdgriga/gdgmaps/tag/TagMapFragment.java
+++ app/src/main/java/lv/gdgriga/gdgmaps/tag/TagMapFragment.java
@@ -1,6 +1,27 @@
 package lv.gdgriga.gdgmaps.tag;
 
-import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.*;
+
+import lv.gdgriga.gdgmaps.Location;
 
 public class TagMapFragment extends MapFragment {
+    private GoogleMap map;
+
+    OnMapReadyCallback onMapReady = new OnMapReadyCallback() {
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            map = googleMap;
+            focusOnRiga();
+        }
+
+        private void focusOnRiga() {
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(Location.RIGA, Location.DEFAULT_ZOOM));
+        }
+    };
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        getMapAsync(onMapReady);
+    }
 }
