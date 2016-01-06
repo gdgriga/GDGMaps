7d214f29e14dc3f476567ca0e5f89cb948c0c448
Valentine Paramonov <valentins.paramonovs@4finance.com>
2016-01-06 10:22:59 +0200

    Setup the tag button

--- app/src/main/java/lv/gdgriga/gdgmaps/tag/TagActivity.java
+++ app/src/main/java/lv/gdgriga/gdgmaps/tag/TagActivity.java
@@ -3,6 +3,8 @@ package lv.gdgriga.gdgmaps.tag;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import lv.gdgriga.gdgmaps.Photo;
 import lv.gdgriga.gdgmaps.PhotoLoader;
@@ -10,6 +12,7 @@ import lv.gdgriga.gdgmaps.R;
 
 public class TagActivity extends Activity {
     private TagMapFragment mapFragment;
+    private Button okButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,7 @@ public class TagActivity extends Activity {
         setContentView(R.layout.tag_activity);
         mapFragment = new TagMapFragment();
         attachFragment(mapFragment);
+        setupButton();
     }
 
     private void attachFragment(TagMapFragment tagMapFragment) {
@@ -40,5 +44,11 @@ public class TagActivity extends Activity {
         Intent toTagPhoto = getIntent();
         String extraName = getString(R.string.photoPath);
         return toTagPhoto.getStringExtra(extraName);
+    }
+
+    private void setupButton() {
+        okButton = (Button) findViewById(R.id.bottom_button);
+        okButton.setText(R.string.tapOnMap);
+        okButton.setEnabled(false);
     }
 }
